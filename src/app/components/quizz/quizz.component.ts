5f19d48c867e28e9d56e75f85ba2c85e5f8d07cc
Apolo Moraes <vlogsapolo@gmail.com>
2024-01-23 19:21:30 -0300

    feat: created game rules

--- src/app/components/quizz/quizz.component.ts
+++ src/app/components/quizz/quizz.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import quizz__questions from '../../../assets/data/quizz_questions.json'
 
 @Component({
   selector: 'app-quizz',
@@ -23,6 +24,30 @@ export class QuizzComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    if(quizz__questions){
+      this.finished = false;
+      this.title = quizz__questions.title;
+
+      this.questions = quizz__questions.questions
+      this.questionSelected = this.questions[this.questionIndex]
+
+      this.questionIndex = 0;
+      this.questionMaxIndex = this.questions.length;
+
+
+  }}
+
+  playerChoose(value: string){
+    this.answers.push(value);
   }
 
+  async nextStep() {
+    this.questionIndex += 1;
+
+    if(this.questionMaxIndex > this.questionIndex){
+      this.questionSelected = this.questions[this.questionIndex];
+    }else{
+      this.finished = true;
+    }
+  }
 }
